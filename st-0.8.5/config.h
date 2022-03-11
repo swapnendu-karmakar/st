b74981c42adbded2c5a0862f39d0187b5f41d5c6
swapnendu-karmakar <ktskarmakar@gmail.com>
2022-03-11 20:49:30 +0530

    dracula color added

--- st-0.8.5/config.h
+++ st-0.8.5/config.h
@@ -115,29 +115,55 @@ float alpha = 0.8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	[0] = "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
-	[1] = "#cc241d", /* red     */
-	[2] = "#98971a", /* green   */
-	[3] = "#d79921", /* yellow  */
-	[4] = "#458588", /* blue    */
-	[5] = "#b16286", /* magenta */
-	[6] = "#689d6a", /* cyan    */
-	[7] = "#a89984", /* white   */
+	/* gruvbox color scheme */
+//	/* 8 normal colors */
+//	[0] = "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
+//	[1] = "#cc241d", /* red     */
+//	[2] = "#98971a", /* green   */
+//	[3] = "#d79921", /* yellow  */
+//	[4] = "#458588", /* blue    */
+//	[5] = "#b16286", /* magenta */
+//	[6] = "#689d6a", /* cyan    */
+//	[7] = "#a89984", /* white   */
+//
+//	/* 8 bright colors */
+//	[8]  = "#928374", /* black   */
+//	[9]  = "#fb4934", /* red     */
+//	[10] = "#b8bb26", /* green   */
+//	[11] = "#fabd2f", /* yellow  */
+//	[12] = "#83a598", /* blue    */
+//	[13] = "#d3869b", /* magenta */
+//	[14] = "#8ec07c", /* cyan    */
+//	[15] = "#ebdbb2", /* white   */
+//
+//  /* special colors */
+//   [256] = "#282828", /* background */
+//  [257] = "#ebdbb2", /* foreground */
+
+	   /* dracula color scheme */
+   	/* 8 normal colors */
+	[0] = "#000000", /* hard contrast: #1d2021 / soft contrast: #32302f */
+	[1] = "#ff5555", /* red     */
+	[2] = "#50fa7b", /* green   */
+	[3] = "#f1fa8c", /* yellow  */
+	[4] = "#bd93f9", /* blue    */
+	[5] = "#ff79c6", /* magenta */
+	[6] = "#8be9fd", /* cyan    */
+	[7] = "#bbbbbb", /* white   */
 
 	/* 8 bright colors */
-	[8]  = "#928374", /* black   */
-	[9]  = "#fb4934", /* red     */
-	[10] = "#b8bb26", /* green   */
-	[11] = "#fabd2f", /* yellow  */
-	[12] = "#83a598", /* blue    */
-	[13] = "#d3869b", /* magenta */
-	[14] = "#8ec07c", /* cyan    */
-	[15] = "#ebdbb2", /* white   */
+	[8]  = "#44475a", /* black   */
+	[9]  = "#ff5555", /* red     */
+	[10] = "#50fa7b", /* green   */
+	[11] = "#f1fa8c", /* yellow  */
+	[12] = "#bd93f9", /* blue    */
+	[13] = "#ff79c6", /* magenta */
+	[14] = "#8be9fd", /* cyan    */
+	[15] = "#ffffff", /* white   */
 
    /* special colors */
-   [256] = "#282828", /* background */
-   [257] = "#ebdbb2", /* foreground */
+   [256] = "#282a36", /* background */
+   [257] = "#f8f8f2", /* foreground */
 
 };
 
